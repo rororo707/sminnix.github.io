98fc64050a20428aea6d9075b9a76d2fabf56244
Shiloh Minnin <rororo808@gmail.com>
2025-01-23 23:51:18 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>
+          Shiloh Minnix
+        </h3>
+        <div>
+          Resume
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;

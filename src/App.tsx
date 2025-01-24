934d61ed976db32f9f49fe9b6b96aa96c4b2e88e
Shiloh Minnin <rororo808@gmail.com>
2025-01-24 00:23:42 -0600

    Include routing?

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
+  return (
+    <Routes>
+      <Route path="/" element=<Body />>
+      </Route>
+
+    </Routes>
+  );
+}
+
+const Body: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
